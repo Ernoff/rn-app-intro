bc0278c7fbf665dd2a0a456ac2de17ec82280d3c
Ernoff <ernest.offiong@gmail.com>
2017-10-16 09:55:59 +0100

    Text formatting in Example folder and update to swiper version

--- Example/index.android.js
+++ Example/index.android.js
@@ -75,26 +75,20 @@ class Example extends Component {
     }
 
     render() {
-        return ( <
-            AppIntro onNextBtnClick = { this.nextBtnHandle }
+        return ( <AppIntro onNextBtnClick = { this.nextBtnHandle }
             onDoneBtnClick = { this.doneBtnHandle }
             onSkipBtnClick = { this.onSkipBtnHandle }
             onSlideChange = { this.onSlideChangeHandle } >
-            <
-            View style = {
+            <View style = {
                 [styles.slide, { backgroundColor: '#fa931d' }] } >
-            <
-            View style = {
+            <View style = {
                 [styles.header, { width: windowsWidth }] } >
-            <
-            View >
-            <
-            Image style = {
+            <View >
+            <Image style = {
                 { width: 75 * 2.5, height: 63 * 2.5 } }
             source = { require('./img/1/c1.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 80,
@@ -104,13 +98,11 @@ class Example extends Component {
                 }
             }
             level = { 20 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 46 * 2.5, height: 28 * 2.5 } }
             source = { require('./img/1/c2.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 23,
@@ -120,13 +112,11 @@ class Example extends Component {
                 }
             }
             level = { 20 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 109 * 2.5, height: 68 * 2.5 } }
             source = { require('./img/1/c5.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 65,
@@ -136,35 +126,24 @@ class Example extends Component {
                 }
             }
             level = { 5 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 23 * 2.5, height: 17 * 2.5 } }
             source = { require('./img/1/c3.png') }
-            /> <
-            /View> <
-            /View> <
-            View style = { styles.info } >
-            <
-            View level = { 10 } > < Text style = { styles.title } > AppIntro < /Text></View >
-            <
-            View level = { 15 } > < Text style = { styles.description } > Pretty Simple Useful in your app tour! < /Text></View >
-            <
-            /View> <
-            /View> <
-            View style = {
+            /> </View> </View> 
+            <View style = { styles.info } >
+            <View level = { 10 } > <Text style = { styles.title } > AppIntro </Text></View>
+            <View level = { 15 } > <Text style = { styles.description } > Pretty Simple Useful in your app tour! </Text></View>
+            </View> </View> 
+            <View style = {
                 [styles.slide, { backgroundColor: '#a4b602' }] } >
-            <
-            View style = {
+            <View style = {
                 [styles.header, { width: windowsWidth }] } >
-            <
-            View >
-            <
-            Image style = {
+            <View >
+            <Image style = {
                 { width: 75 * 2.5, height: 63 * 2.5 } }
             source = { require('./img/2/1.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 30,
@@ -174,13 +153,11 @@ class Example extends Component {
                 }
             }
             level = { 20 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 101 * 2.5, height: 71 * 2.5 } }
             source = { require('./img/2/2.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 10,
@@ -190,28 +167,19 @@ class Example extends Component {
                 }
             }
             level = {-20 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 85 * 2.5, height: 73 * 2.5 } }
             source = { require('./img/2/3.png') }
-            /> <
-            /View> <
-            /View> <
-            View style = { styles.info } >
-            <
-            View level = { 10 } > < Text style = { styles.title } > Title! < /Text></View >
-            <
-            View level = { 15 } > < Text style = { styles.description } > description! < /Text></View >
-            <
-            /View> <
-            /View> <
-            View style = {
+            /> </View> </View> 
+            <View style = { styles.info } >
+            <View level = { 10 } > <Text style = { styles.title } > Title! </Text></View>
+            <View level = { 15 } > <Text style = { styles.description } > description! </Text></View>
+            </View> </View> 
+            <View style = {
                 [styles.slide, { backgroundColor: '#406E9F' }] } >
-            <
-            View style = {
+            <View style = {
                 [styles.header, { width: windowsWidth }] } >
-            <
-            View style = {
+            <View style = {
                 {
                     position: 'absolute',
                     top: 20,
@@ -220,13 +188,11 @@ class Example extends Component {
                     height: windowsHeight,
                 }
             } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 138 * 2.5, height: 83 * 2.5 } }
             source = { require('./img/3/3.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 25,
@@ -236,20 +202,16 @@ class Example extends Component {
                 }
             }
             level = {-15 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 103 * 2.5, height: 42 * 2.5 } }
             source = { require('./img/3/4.png') }
-            /> <
-            /View> <
-            View level = { 10 } >
-            <
-            Image style = {
+            /> </View> 
+            <View level = { 10 } >
+            <Image style = {
                 { width: 95 * 2.5, height: 55 * 2.5 } }
             source = { require('./img/3/1.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 65,
@@ -259,28 +221,19 @@ class Example extends Component {
                 }
             }
             level = { 25 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 47 * 2.5, height: 43 * 2.5 } }
             source = { require('./img/3/2.png') }
-            /> <
-            /View> <
-            /View> <
-            View style = { styles.info } >
-            <
-            View level = { 10 } > < Text style = { styles.title } > Title! < /Text></View >
-            <
-            View level = { 15 } > < Text style = { styles.description } > description! < /Text></View >
-            <
-            /View> <
-            /View> <
-            View style = {
+            /> </View> </View> 
+            <View style = { styles.info } >
+            <View level = { 10 } > <Text style = { styles.title } > Title! </Text></View>
+            <View level = { 15 } > <Text style = { styles.description } > description! </Text></View>
+            </View> </View> 
+            <View style = {
                 [styles.slide, { backgroundColor: '#DB4302' }] } >
-            <
-            View style = {
+            <View style = {
                 [styles.header, { width: windowsWidth }] } >
-            <
-            View style = {
+            <View style = {
                 {
                     position: 'absolute',
                     top: 25,
@@ -290,20 +243,16 @@ class Example extends Component {
                 }
             }
             level = { 15 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 96 * 2.5, height: 69 * 2.5 } }
             source = { require('./img/4/4.png') }
-            /> <
-            /View> <
-            View >
-            <
-            Image style = {
+            /> </View> 
+            <View>
+            <Image style = {
                 { width: 50 * 2.5, height: 63 * 2.5 } }
             source = { require('./img/4/1.png') }
-            /> <
-            /View> <
-            View style = {
+            /> </View> 
+            <View style = {
                 {
                     position: 'absolute',
                     top: 20,
@@ -313,22 +262,15 @@ class Example extends Component {
                 }
             }
             level = { 20 } >
-            <
-            Image style = {
+            <Image style = {
                 { width: 46 * 2.5, height: 98 * 2.5 } }
             source = { require('./img/4/3.png') }
-            /> <
-            /View> <
-            /View> <
-            View style = { styles.info } >
-            <
-            View level = { 10 } > < Text style = { styles.title } > Title! < /Text></View >
-            <
-            View level = { 15 } > < Text style = { styles.description } > description! < /Text></View >
-            <
-            /View> <
-            /View> <
-            /AppIntro>
+            /> </View> </View> 
+            <View style = { styles.info } >
+            <View level = { 10 } > <Text style = { styles.title } > Title! </Text></View>
+            <View level = { 15 } > <Text style = { styles.description } > description! </Text></View>
+            </View> </View> 
+            </AppIntro>
         );
     }
 }
